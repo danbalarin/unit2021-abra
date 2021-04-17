a9f8d97850744b6ec309fb6af85a7b9bab18d9f9
Dan Balarin <dan.balarin@gmail.com>
2021-04-17 14:26:43 +0200

    fix mocks

--- app/src/models/Reservation.ts
+++ app/src/models/Reservation.ts
@@ -9,32 +9,32 @@ export interface Reservation {
 export const RESERVATION: Reservation[] = [
   {
     userId: 56,
-    from: new Date("2021-04-17T08:00:00+00:00"),
-    to: new Date("2021-04-17T18:00:00+00:00"),
+    from: "2021-04-17T08:00:00+00:00",
+    to: "2021-04-17T18:00:00+00:00",
     parkingPlaceId: 105,
   },
   {
     userId: 56,
-    from: new Date("2021-04-18T08:00:00+00:00"),
-    to: new Date("2021-04-18T18:00:00+00:00"),
+    from: "2021-04-18T08:00:00+00:00",
+    to: "2021-04-18T18:00:00+00:00",
     parkingPlaceId: 105,
   },
   {
     userId: 41,
-    from: new Date("2021-04-19T08:00:00+00:00"),
-    to: new Date("2021-04-19T18:00:00+00:00"),
+    from: "2021-04-19T08:00:00+00:00",
+    to: "2021-04-19T18:00:00+00:00",
     parkingPlaceId: 104,
   },
   {
     userId: 41,
-    from: new Date("2021-04-19T08:00:00+00:00"),
-    to: new Date("2021-04-19T18:00:00+00:00"),
+    from: "2021-04-19T08:00:00+00:00",
+    to: "2021-04-19T18:00:00+00:00",
     parkingPlaceId: 102,
   },
   {
     userId: 41,
-    from: new Date("2021-04-18T08:00:00+00:00"),
-    to: new Date("2021-04-18T18:00:00+00:00"),
+    from: "2021-04-18T08:00:00+00:00",
+    to: "2021-04-18T18:00:00+00:00",
     parkingPlaceId: 106,
   },
 ];
