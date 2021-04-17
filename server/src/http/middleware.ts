4faaca6875a33ed0aefc6467b13f05a7d53f21ac
Honza Charamza <jenda60@gmail.com>
2021-04-17 13:50:54 +0200

    backend body parse json

--- server/src/http/middleware.ts
+++ server/src/http/middleware.ts
@@ -7,7 +7,11 @@ import { Config } from '../utils/config';
 
 export function applyMiddlewares(app: Express, config: Config) {
   app.use(morgan('tiny'));
-  app.use(json());
+  app.use(json({ 
+    type(req) {
+      return true;
+    }
+  }));
   app.use(helmet());
   app.use(cors());
   app.use('/flexibee', proxy(config.flexibee.proxyUrl));
